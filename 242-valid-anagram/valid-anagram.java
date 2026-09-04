a663465f5f30ee5cca7af2856165b3413acf6fdd
roshankoshyjose <75484647+roshankoshyjose@users.noreply.github.com>
2026-09-04 23:22:07 +0530

    Time: 13 ms (28.24%) | Memory: 46.4 MB (34.21%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> countS = new HashMap<>();
+        Map<Character, Integer> countT = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            countS.put(c, countS.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            countT.put(c, countT.getOrDefault(c, 0) + 1);
+        }
+
+        return countS.equals(countT);
+    }
+}
